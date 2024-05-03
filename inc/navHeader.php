e2d0425bbf1e35311d94754c6762110a2292283c
unknown <sudayonfernando01@gmail.com>
2024-05-03 16:59:47 +0800

    styles

---
+++ inc/navHeader.php
@@ -0,0 +1,17 @@
+<div class="navbar">
+    <div class="navHead flex-nav">
+        <div class="nav-header">
+            <span><a href="../index.php"><img src="../asset/logo.png"></a></span>
+            <button class="nav-toggle" id="navToggle">
+                <i class="fas fa-bars"></i>
+            </button>
+        </div>
+        <nav class="nav-links" id="nav-links">
+            <ul>
+                <li class="nav-link"><a href="#">About</a></li>
+                <li class="nav-link"><a href="#">Store</a></li>
+                <li class="nav-link"><a href="#">Products</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
--- view/oxc-about.php
+++ view/oxc-about.php
@@ -8,6 +8,7 @@
     <!-- <link rel="stylesheet" href="../styles/styles.css"> -->
     <link rel="stylesheet" href="../styles/nav.css">
     <link rel="stylesheet" href="../styles/about.css">
+    <link rel="stylesheet" href="../styles/footer.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -16,25 +17,7 @@
 </head>
 <body>
 <!-- <div class="wrap"> -->
-    
-<div class="navbar">
-    <div class="navHead flex-nav">
-        <div class="nav-header">
-            <span><a href="../index.php"><img src="../asset/logo.png"></a></span>
-            <button class="nav-toggle" id="navToggle">
-                <i class="fas fa-bars"></i>
-            </button>
-        </div>
-        <nav class="nav-links" id="nav-links">
-            <ul>
-                <li class="nav-link"><a href="#">About</a></li>
-                <li class="nav-link"><a href="#">Store</a></li>
-                <li class="nav-link"><a href="#">Products</a></li>
-            </ul>
-        </nav>
-    </div>
-</div>
-
+<?php include '../inc/navHeader.php' ?>
     
 <header>
 
@@ -62,7 +45,40 @@
     </p>
 </section>
 
-    <?php include '../inc/footer.php'; ?>
+
+<section class="content-2">
+    <!-- <div class="wallpaper">
+        <img src="../asset/banner.jpg" alt="">
+    </div> -->
+    <div class="para">
+        POWERCLEAN Solutions is a brand that caters to the needs of Small & Medium Enterprises (SME).
+    </div>
+</section>
+
+<section class="content-2">
+    <div class="para">
+        Our CORE PRODUCTS are LAUNDRY chemicals, which gained the trust of most Laundry Businesses in the Philippines.
+    </div>
+</section>
+
+<section class="content-4">
+    <div class="row">
+        <div class="column certificate">
+            <div class="para">
+                <br><h1>YES, WE ARE CERTIFIED</h1><br><br>
+                <img src='../asset/oxychem.png'>
+            </div>
+        </div>
+
+        <div class="column certificate">
+            <div class="para">
+                <img src='../asset/ISOxUKAS.jpg'>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include '../inc/footer.php'; ?>
 
 
 <script src="../asset/js/nav.js"></script>
